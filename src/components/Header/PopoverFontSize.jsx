e71e131bf6df4bbb8b919924c5388bb7e28e8620
suuzanemoura <suuzanemoura@gmail.com>
2023-12-18 12:42:38 -0300

    feat: adicionando overlay no header

--- src/components/Header/PopoverFontSize.jsx
+++ src/components/Header/PopoverFontSize.jsx
@@ -3,7 +3,7 @@ import { Popover, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Fragment, useContext } from "react";
 
-export default function PopoverFontSize({ position, iconSize }) {
+export default function PopoverFontSize({ position }) {
   const { setTextSize, sizesVariation } = useContext(FontContext);
   return (
     <Popover className="relative">
@@ -33,6 +33,7 @@ export default function PopoverFontSize({ position, iconSize }) {
           aria-hidden="true"
         />
       </Popover.Button>
+      <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
 
       <Transition
         as={Fragment}
@@ -48,29 +49,31 @@ export default function PopoverFontSize({ position, iconSize }) {
         >
           <div className="p-2 sm:p-4">
             {sizesVariation.map((item) => (
-              <div
+              <Popover.Button
                 key={item.name}
-                className="group relative flex items-center gap-x-6 rounded-lg  p-4  text-sm leading-4 hover:bg-gray-50 cursor-pointer"
                 onClick={() => setTextSize(item.fontSize)}
+                className={"w-full text-left"}
               >
-                <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 -960 960 960"
-                    className={`${item.iconSize} text-gray-600 group-hover:text-indigo-600`}
-                    aria-hidden="true"
-                  >
-                    <path d="m159-280 150-400h72l150 400h-69l-36-102H264l-36 102h-69Zm126-160h120l-58-166h-4l-58 166Zm369 171q-49 0-77-25.5T549-364q0-42 32.5-68.5T665-459q23 0 42.5 3.5T741-444v-14q0-27-18.5-43T672-517q-21 0-39.5 9T601-482l-43-32q19-27 48-41t67-14q62 0 95 29.5t33 85.5v176h-59v-34h-3q-13 20-35 31.5T654-269Zm10-50q32 0 54.5-22.5T741-396q-14-8-32-12t-33-4q-32 0-49 12.5T610-364q0 20 15 32.5t39 12.5Z" />
-                  </svg>
+                <div className="group relative flex items-center gap-x-6 rounded-lg  p-4  text-sm leading-4 hover:bg-gray-50 cursor-pointer">
+                  <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 -960 960 960"
+                      className={`${item.iconSize} text-gray-600 group-hover:text-indigo-600`}
+                      aria-hidden="true"
+                    >
+                      <path d="m159-280 150-400h72l150 400h-69l-36-102H264l-36 102h-69Zm126-160h120l-58-166h-4l-58 166Zm369 171q-49 0-77-25.5T549-364q0-42 32.5-68.5T665-459q23 0 42.5 3.5T741-444v-14q0-27-18.5-43T672-517q-21 0-39.5 9T601-482l-43-32q19-27 48-41t67-14q62 0 95 29.5t33 85.5v176h-59v-34h-3q-13 20-35 31.5T654-269Zm10-50q32 0 54.5-22.5T741-396q-14-8-32-12t-33-4q-32 0-49 12.5T610-364q0 20 15 32.5t39 12.5Z" />
+                    </svg>
+                  </div>
+                  <div className="flex-auto">
+                    <p className="block font-medium text-base text-gray-900">
+                      {item.name}
+                    </p>
+                    <span className="absolute inset-0" />
+                    <p className="mt-1 text-gray-600">{item.description}</p>
+                  </div>
                 </div>
-                <div className="flex-auto">
-                  <p className="block font-medium text-base text-gray-900">
-                    {item.name}
-                  </p>
-                  <span className="absolute inset-0" />
-                  <p className="mt-1 text-gray-600">{item.description}</p>
-                </div>
-              </div>
+              </Popover.Button>
             ))}
           </div>
         </Popover.Panel>
