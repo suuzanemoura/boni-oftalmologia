0f77c3b59a3151f5ed398b720cef0a3694d8402b
suuzanemoura <suuzanemoura@gmail.com>
2024-01-08 16:16:07 -0300

    refactor: atualização disclaimer para 2024

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -117,7 +117,7 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-center">
           <p className="text-sm text-gray-500 sm:text-center">
-            © 2023 Dr. Pedro Boni - Oftalmologista. Todos os direitos
+            © 2024 Dr. Pedro Boni - Oftalmologista. Todos os direitos
             reservados.
           </p>
         </div>
