234942eb2190b1db53485af265162f49e3393f78
suuzanemoura <suuzanemoura@gmail.com>
2024-01-26 15:14:13 -0300

    feat/refactor: finalização do youtube section

--- src/components/AlertError/AlertError.jsx
+++ src/components/AlertError/AlertError.jsx
@@ -4,13 +4,13 @@ export default function AlertError({ title, children }) {
   return (
     <>
       <div
-        class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+        className="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
         role="alert"
       >
-        <ExclamationCircleIcon class="flex-shrink-0 inline w-5 h-5 me-3" />
-        <span class="sr-only">Atenção!</span>
+        <ExclamationCircleIcon className="flex-shrink-0 inline w-5 h-5 me-3" />
+        <span className="sr-only">Atenção!</span>
         <div>
-          <span class="font-medium">{title}</span>
+          <span className="font-medium">{title}</span>
           {children}
         </div>
       </div>
--- src/components/YoutubeSection/YoutubePlaylist/YoutubePlaylist.jsx
+++ src/components/YoutubeSection/YoutubePlaylist/YoutubePlaylist.jsx
@@ -20,40 +20,47 @@ export default function YoutubePlaylist({ id, apiKey }) {
     }
   }, [loaded, playlistItemsApiInfo]);
 
-  if (isLoading) {
-    return <LoadingCircle />;
-  }
-  if (error) {
-    return (
-      <AlertError title={"Atenção! Vídeos não foram foram carregados:"}>
-        <ul className="mt-1.5 list-disc list-inside">
-          <li>Por favor, verifique sua internet.</li>
-          <li>Depois recarregue a página e tente novamente.</li>
-        </ul>
-      </AlertError>
-    );
-  }
-
-  if (!playlistItemsInfo?.items) return <p>No videos data</p>;
-
   return (
-    <Carousel
-      width={
-        window.screen.width < 360
-          ? 240
-          : window.screen.width === 360 && window.screen.width < 480
-          ? 320
-          : 366.63
-      }
-    >
-      {playlistItemsInfo?.items?.map((video) => {
-        return (
-          <YoutubeVideoCard
-            key={video.id}
-            videoId={video.snippet.resourceId.videoId}
-          />
-        );
-      })}
-    </Carousel>
+    <>
+      {error ? (
+        <AlertError title={"Atenção! Vídeos não foram foram carregados:"}>
+          <ul className="mt-1.5 list-disc list-inside">
+            <li>Por favor, verifique sua internet.</li>
+            <li>Depois recarregue a página e tente novamente.</li>
+          </ul>
+        </AlertError>
+      ) : isLoading ? (
+        <LoadingCircle />
+      ) : loaded ? (
+        <Carousel
+          width={
+            window.screen.width < 360
+              ? 240
+              : window.screen.width === 360 && window.screen.width < 480
+              ? 320
+              : 366.63
+          }
+        >
+          {playlistItemsInfo?.items?.map((video) => {
+            return (
+              <YoutubeVideoCard
+                key={video.id}
+                videoId={video.snippet.resourceId.videoId}
+              />
+            );
+          })}
+        </Carousel>
+      ) : (
+        <AlertError title={"Vídeos não encontrados:"}>
+          <ul className="mt-1.5 list-disc list-inside">
+            <li>
+              Não foram encontrados vídeos associados ao canal solicitado.
+            </li>
+            <li>Verifique sua conexão com a intenet.</li>
+            <li>Recarregue a página e tente novamente.</li>
+          </ul>
+        </AlertError>
+      )}
+    </>
   );
 }
