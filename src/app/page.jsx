8e95307ce8e662e6190a5075b14abcb1a449e847
suuzanemoura <suuzanemoura@gmail.com>
2024-01-31 15:57:10 -0300

    feat: clinica section desenvolvida

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import ServicesSection from "@/src/components/ServiceSection/ServicesSection";
 import BioSection from "../components/BioSection/BioSection";
 import YoutubeSection from "../components/YoutubeSection/YoutubeSection";
 import { apiConfig } from "../config";
+import ClinicSection from "../components/ClinicSection/ClinicSection";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <ServicesSection />
         <BioSection />
         <YoutubeSection apiConfig={apiConfig} />
+        <ClinicSection />
       </main>
       <Footer />
     </>
