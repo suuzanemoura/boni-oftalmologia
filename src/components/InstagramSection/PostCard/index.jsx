6f2a3ff1f6c166533e4656849eb4377cec182171
suuzanemoura <suuzanemoura@gmail.com>
2024-02-29 18:10:32 -0300

    refactor: trocando user test para user final do instagram

--- src/components/InstagramSection/PostCard/index.jsx
+++ src/components/InstagramSection/PostCard/index.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function PostCard({ post }) {
+  console.log(post);
   return (
     <Link
       href={post.permalink}
@@ -9,13 +10,23 @@ export default function PostCard({ post }) {
     >
       <div className="text-center w-[300px] h-[300px] snap-start">
         <div className="w-[300px] h-[300px] relative">
-          <Image
-            src={post.media_url}
-            alt={`Publicação no perfil do Instagram do ${post.username}. Segue o link: ${post.permalink}`}
-            className="w-full h-full object-cover rounded-xl absolute"
-            fill={true}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
+          {post.media_type !== "VIDEO" ? (
+            <Image
+              src={post.media_url}
+              alt={`Publicação no perfil do Instagram do ${post.username}. Segue o link: ${post.permalink}`}
+              className="w-full h-full object-cover rounded-xl absolute"
+              fill={true}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          ) : (
+            <Image
+              src={post.thumbnail_url}
+              alt={`Publicação no perfil do Instagram do ${post.username}. Segue o link: ${post.permalink}`}
+              className="w-full h-full object-cover rounded-xl absolute object-center"
+              fill={true}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          )}
         </div>
       </div>
     </Link>
